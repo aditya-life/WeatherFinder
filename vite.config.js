deec05132be0ff45c78dcd3968d1b472e181b854
Aditya Kumar <aditya12186@gmail.com>
2025-10-18 10:29:54 +0530

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    allowedHosts: 'all',
+    // allow all hosts (only for dev / trusted env)
+    allowedHosts: true,
   },
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: true,
+  }
 })
